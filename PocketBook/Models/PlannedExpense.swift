9c1ca2798105f824c3bc8e3aee4dfd94e970811d
Betzer09 <31580350+Betzer09@users.noreply.github.com>
2017-11-07 10:56:56 -0700

    Created PlannedExpense Model

---
+++ PocketBook/Models/PlannedExpense.swift
@@ -0,0 +1,84 @@
+//
+//  PlannedExpense.swift
+//  PocketBook
+//
+//  Created by Austin Betzer on 11/7/17.
+//  Copyright © 2017 SPARQ. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+struct PlannedExpense {
+    
+    // MARK: - Keys
+    static let recordType = "PlannedExpense"
+    static let nameKey = "name"
+    static let accountKey = "account"
+    static let dueDateKey = "dueDate"
+    static let initialAmountKey = "initialAmount"
+    static let goalAmountKey = "goalAmount"
+    static let totalSavedKey = "totalSaved"
+    
+    // MARK: - Properties
+    let name: String
+    let account: String
+    let dueDate: Date
+    let initialAmount: Double
+    let goalAmount: Double
+    var totalSaved: Double {
+        
+        // initial amount Plus the income for the planned expense -> income dictionary
+        // TODO: Create a income dictionary
+        return initialAmount + 0
+    }
+    
+    // MARK: - Init
+    init(name: String, account: String, dueDate: Date, initialAmount: Double, goalAmount: Double) {
+        
+        self.name = name
+        self.account = account
+        self.dueDate = dueDate
+        self.initialAmount = initialAmount
+        self.goalAmount = goalAmount
+        
+    }
+    
+    // MARK: - cloudKitRecord PUT
+    
+    var cloudKitRecord: CKRecord {
+        let record = CKRecord(recordType: PlannedExpense.recordType)
+        
+        record.setValue(name, forKey: PlannedExpense.nameKey)
+        record.setValue(account, forKey: PlannedExpense.accountKey)
+        record.setValue(dueDate, forKey: PlannedExpense.dueDateKey)
+        record.setValue(initialAmount, forKey: PlannedExpense.initialAmountKey)
+        record.setValue(goalAmount, forKey: PlannedExpense.goalAmountKey)
+        record.setValue(totalSaved, forKey: PlannedExpense.totalSavedKey)
+        
+        return record
+        
+    }
+    
+    // MARK: - failable Initializer cloudKit
+    
+    init?(cloudKitRecord: CKRecord) {
+        guard let name = cloudKitRecord[PlannedExpense.nameKey] as? String,
+            let account = cloudKitRecord[PlannedExpense.accountKey] as? String,
+            let dueDate = cloudKitRecord[PlannedExpense.dueDateKey] as? Date,
+            let initialAmount = cloudKitRecord[PlannedExpense.initialAmountKey] as? Double,
+            let goalAmount = cloudKitRecord[PlannedExpense.goalAmountKey] as? Double,
+            let totalSaved = cloudKitRecord[PlannedExpense.totalSavedKey] as? Double else {return nil}
+        
+        
+        self.name = name
+        self.account = account
+        self.dueDate = dueDate
+        self.initialAmount = initialAmount
+        self.goalAmount = goalAmount
+    }
+    
+}
+
+
+
