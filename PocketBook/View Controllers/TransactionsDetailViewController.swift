ac48ad93cb5ab8d1057884d8d816d70e5a205d0d
Betzer09 <31580350+Betzer09@users.noreply.github.com>
2017-11-30 16:26:45 -0700

    Updated transactions to pop the view controller when it is a transaction and segue when it is a planned expense

--- PocketBook/View Controllers/TransactionsDetailViewController.swift
+++ PocketBook/View Controllers/TransactionsDetailViewController.swift
@@ -74,8 +74,8 @@ class TransactionsDetailViewController: UIViewController, UIPickerViewDelegate, 
         
         guard let navController = parent as? UINavigationController,
             let tabBarController = navController.parent as? UITabBarController else { return }
-        
         tabBarController.selectedIndex = 2
+        
         saveTransaction()
 
     }
@@ -480,7 +480,11 @@ class TransactionsDetailViewController: UIViewController, UIPickerViewDelegate, 
         } else {
             createTransaction()
         }
+        if plannedExpenseTransaction != nil {
         performSegue(withIdentifier: "unwindToPlannedExpenseVC", sender: self)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
     }
     
     private func createTransaction() {
